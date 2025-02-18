2a961ee7e3ec8e13561947e1a7324c702a015dce
Vaibhav <vaibhavpant2004@gmail.com>
2025-02-18 11:54:26 +0530

    still cors error ...

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,10 @@ const app = express();
 // Middleware
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,
+        origin: [process.env.CORS_ORIGIN],
         credentials: true,
-        methods: 'GET,POST,PUT,DELETE',
-        allowedHeaders: 'Content-Type, Authorization'
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']    
     })
 );
 app.use(express.json());
