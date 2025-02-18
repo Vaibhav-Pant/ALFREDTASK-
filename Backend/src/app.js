0a84a997f6e319e6d59d322b8f44d15e1adfab21
Vaibhav <vaibhavpant2004@gmail.com>
2025-02-18 11:57:11 +0530

    f cors error ..

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,41 @@
 import express from "express"
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 
 // Middleware
-app.use(
-    cors({
-        origin: [process.env.CORS_ORIGIN],
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization']    
-    })
-);
+// app.use(
+//     cors({
+//         origin: [process.env.CORS_ORIGIN],
+//         credentials: true,
+//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//         allowedHeaders: ['Content-Type', 'Authorization']    
+//     })
+// );
+const allowedOrigins = [process.env.CORS_ORIGIN];
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }else {
+        // Respond with 403 Forbidden if the origin is not allowed
+        return res.status(403).json({ message: 'Forbidden' });
+      }
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Handle preflight OPTIONS request
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+    
+    next();
+  });
+
+
 app.use(express.json());
 
 
