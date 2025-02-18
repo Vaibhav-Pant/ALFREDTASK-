635575e8fd61cd7f8b3948e6874e3a29c47dd090
Vaibhav <vaibhavpant2004@gmail.com>
2025-02-18 11:51:28 +0530

    cors...

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
+        credentials: true,
+        methods: 'GET,POST,PUT,DELETE',
+        allowedHeaders: 'Content-Type, Authorization'
     })
 );
 app.use(express.json());
